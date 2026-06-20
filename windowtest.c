7e7ddef9f13ddf81dd1d617c6f80bae356a45311
k-ef <faurillokarledward@gmail.com>
2026-06-20 12:09:01 +0800

    Second commit
    Drawing pixels to a window. To be continued.

--- windowtest.c
+++ windowtest.c
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 bool quit = false;
 
@@ -7,11 +8,11 @@ struct {
     int width;
     int height;
     uint32_t *pixels;    
-} frame = {0}
+} frame = {0};
 
-BITMAPINFO frame_bitmap_info;
-HBITMAP frame_bitmap = 0;
-HDC frame_device_context = 0;
+BITMAPINFO frame_bitmap_info; //bitmap info (metadata/header)
+HBITMAP frame_bitmap = 0; //handle to reference a bitmap object in memory
+HDC frame_device_context = 0; //device context
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -26,6 +27,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
     RegisterClass(&wc);
 
     frame_bitmap_info.bmiHeader.biSize = sizeof(frame_bitmap_info.bmiHeader);
+    frame_bitmap_info.bmiHeader.biPlanes = 1; //set to 1 always
+    frame_bitmap_info.bmiHeader.biBitCount = 32; //bitdepth, bits per pixel
+    frame_bitmap_info.bmiHeader.biCompression = BI_RGB;
+    frame_device_context = CreateCompatibleDC(0);
 
     HWND hwnd = CreateWindowEx(
         0,                      //Optional window style
@@ -54,8 +59,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
             TranslateMessage(&message);
             DispatchMessage(&message);
         }
-        
-        // Do game stuff here
+
+        unsigned int p = 0;
+        frame.pixels[(p++)%(frame.width*frame.height)] = Rand32();
+        frame.pixels[Rand32()%(frame.width*frame.height)] = 0;
+
+        InvalidateRect(hwnd, NULL, FALSE); //marks a section of the window as invalid and needing to be redrawn
+        UpdateWindow(hwnd); //updates a wm_paint message to window process message
     }
 }
 
@@ -66,6 +76,33 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         case WM_DESTROY: {
             quit = true;
         } break;
+
+        case WM_PAINT: { //all window drawing (as in how the window is painted not the canvas)
+            PAINTSTRUCT paint;
+            HDC device_context;
+            device_context = BeginPaint(hwnd, &paint);
+            BitBlt(device_context,
+                   paint.rcPaint.left, paint.rcPaint.top,
+                   paint.rcPaint.right - paint.rcPaint.left, paint.rcPaint.bottom - paint.rcPaint.top,
+                   frame_device_context, //device context of pixel array
+                   paint.rcPaint.left, paint.rcPaint.top, //top left corner to start copying from
+                   SRCCOPY); //raster operation code (copy array pixels to window pixels)
+            EndPaint(hwnd, &paint);
+        } break;
+
+        case WM_SIZE: { 
+            frame_bitmap_info.bmiHeader.biWidth = LOWORD(lParam);
+            frame_bitmap_info.bmiHeader.biHeight = HIWORD(lParam);
+
+            if (frame_bitmap) {
+                DeleteObject(frame_bitmap);
+            }
+            frame_bitmap = CreateDIBSection(NULL, &frame_bitmap_info, DIB_RGB_COLORS, &frame.pixels, 0, 0);
+            SelectObject(frame_device_context, frame_bitmap);
+
+            frame.width = LOWORD(lParam);
+            frame.height = HIWORD(lParam);
+        }
 
         default: {
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
