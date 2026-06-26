bc1667c6703f7667303152f5d42ce756945d6019
k-ef <faurillokarledward@gmail.com>
2026-06-26 13:42:13 +0800

    Menu test file
    Trying out menus before implementing into main program.

---
+++ menutest.c
@@ -0,0 +1,111 @@
+#define UNICODE //DONT FORGET TO INCLUDE UNICODE
+#define _UNICODE
+#include <stdio.h>
+#include <windows.h>
+#include <stdbool.h>
+
+bool quit = false; //Handles termination of main loop
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void AddMenus(HWND hWnd);
+
+HMENU hMenu; //Menu Handler
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
+{
+    
+    //1. Create Window Class
+    WNDCLASS wc = {0};
+    const wchar_t CLASS_NAME[] = L"PaintClass";
+
+    //1.1 Initialize mandatory members
+    wc.lpszClassName = CLASS_NAME; //Long string 
+    wc.lpfnWndProc = WindowProcedure; //Pass address of window procedure and define function
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+    //2. Register Window Class
+    if (!RegisterClass(&wc)) {
+        return -1;
+    }
+
+    //3. Create Window and store handle
+    HWND hWnd = CreateWindow(
+        CLASS_NAME,          //Window Class name
+        L"Paint in C",          //Window name
+        WS_OVERLAPPEDWINDOW,    //Window style
+        CW_USEDEFAULT,          //X-coordinate of the upper left corner of the window
+        CW_USEDEFAULT,          //Y-coordinate of the upper left corner of the window
+        500,                    //Window width
+        500,                    //Window height
+        NULL,                   //Handle of the parent window (NULL because independent window)
+        NULL,                   //Handle to a menu
+        hInstance,              //Handle to the instance of the module to be associated with the window
+        NULL                   //LPVOID (idk, not important yet)
+    );
+
+    if (hWnd == NULL) {
+        return -1;
+    }
+
+    //4. Show window
+    ShowWindow(hWnd, nCmdShow);
+
+    //5. Create program loop and message loop
+    while (!quit) {
+        MSG msg = {0};
+        while (GetMessage(&msg, NULL, 0, 0)) {
+            TranslateMessage(&msg); //Translates virtual-key messages into character messsages
+            DispatchMessage(&msg);  //Dispatches message to a window procedure
+        }
+        //MAIN LOOP
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    //1.2 Create switch statements for messages
+    switch (uMsg) {
+        case WM_COMMAND: { //Will run when user invokes a command item from a menu, etc...
+            switch (wParam) { //Contains ID of the parameter (Menu that was clicked/processed)
+                case 1: {
+                    MessageBeep(MB_OK);
+                } break;
+            }
+        } break;
+
+        case WM_CREATE: { //Send when application requests that a window be created
+            AddMenus(hWnd);
+        } break;
+
+        case WM_QUIT:
+        case WM_DESTROY: {
+            // PostQuitMessage(0); //Indicates to system that thread has made a request to quit (exit code 0)  
+            quit = true; //Will terminate the main loop
+        } break;
+
+        default: {//Default function to take care of the messages
+            return DefWindowProcW(hWnd, uMsg, wParam, lParam);
+        } break;
+    }
+}
+
+void AddMenus(HWND hWnd)
+{
+    //CREATE MENU
+    hMenu = CreateMenu(); 
+
+    //APPEND ITEMS TO MENU
+    //1. Handle to menu
+    //2. Appearance and behavior of menu item
+    //3. Identifier of the new menu item
+    //4. Content of menu item (e.g. bitmap handle if MF_BITMAP)
+    //When pressed, a message (WM_COMMAND) will be passed to the message loop process the menu
+    //The identity of the menu clicked will be passed in the wParam of the window procedure 
+    AppendMenu(hMenu, MF_STRING, 1, L"Bruh");
+
+    //SET MENU TO WINDOW
+    SetMenu(hWnd, hMenu);
+}
